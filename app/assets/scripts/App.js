8235ec834a40d3f239954260fb23203cfd843d96
mahmud1rahman <mahmud_ur_rahman@@hotmail.com>
2020-02-20 21:20:28 +0000

    menu content with js nearly finishing

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,23 +1,10 @@
 import "../styles/styles.css";
-import Person from "./modules/Person";
+import MobileMenu from "./modules/MobileMenu";
+
+let mobileMenu = new MobileMenu();
 
 if (module.hot) {
   module.hot.accept();
 }
 
-/* lession example code below this line */
 
-/*contructor function/BluePrint8*/
-class Adult extends Person {
-  payTaxes() {
-    console.log(`${this.name} owes £1000,boom,boom!`);
-  }
-}
-
-let rana = new Person("Rana miah", "red ");
-rana.greet();
-rana.color;
-
-let mahmud = new Adult("Mahmud Rahman", "blue");
-mahmud.greet();
-mahmud.payTaxes();
